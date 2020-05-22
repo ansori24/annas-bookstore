e2184f7e3cfad2279e25af548b1062260cce4d2d
Misbah ansori <misbah.ansori24@gmail.com>
2020-05-22 09:01:46 +0700

    Testing error handling response

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,8 @@
 namespace App\Exceptions;
 
 use Throwable;
+use Illuminate\Support\Str;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
@@ -76,5 +78,46 @@ class Handler extends ExceptionHandler
             ->values();
 
         return response()->json(['errors' => $errors], $exception->status);
+    }
+
+    /**
+     * Prepare a JSON response for the given exception.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Throwable  $e
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function prepareJsonResponse($request, Throwable $e)
+    {
+        return response()->json([
+            'errors' => [
+                [
+                    'title' => Str::of(class_basename($e))->snake(' ')->title()->__toString(),
+                    'details' => $e->getMessage()
+                ]
+            ]
+        ], $this->isHttpException($e) ? $e->getStatusCode() : 500);
+    }
+
+    /**
+     * Convert an authentication exception into a response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Auth\AuthenticationException  $exception
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        if ($request->expectsJson()) {
+            return response()->json([
+                'errors' => [
+                    [
+                        'title' => 'Unauthenticated',
+                        'details' => 'You are not authenticated'
+                    ]
+                ]
+            ], 403);
+        }
+        return redirect()->guest($exception->redirectTo() ?? route('login'));
     }
 }
